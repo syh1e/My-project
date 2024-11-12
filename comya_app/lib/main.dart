32c335894edb07dcfa13f48446ee945c76a030d5
syh1e <marioshin04@gmail.com>
2024-11-13 00:45:20 +0900

    test

--- comya_app/lib/main.dart
+++ comya_app/lib/main.dart
@@ -11,9 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(),
-      ),
+      home: Scaffold(),
     );
   }
 }
